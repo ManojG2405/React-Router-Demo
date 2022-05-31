5bf351b818924958aff60474a87c6ff796af87a7
ManojG2405 <Gawade.manoj77@gmail.com>
2022-05-31 08:05:09 +0530

    Completed

---
+++ src/Pages/Contact.js
@@ -0,0 +1,11 @@
+
+function Contact() {
+    return (
+      <div className="alert alert-info" role="alert">
+        <h1>Contact Us</h1>
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
